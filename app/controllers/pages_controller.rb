de2ab0ca75879be247d1bd66703ecc9f97971ffc
Angel Vanegas <angelman@gmail.com>
2020-06-11 20:01:39 -0400

    working upload

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,28 @@
 class PagesController < ApplicationController
   def index
+    @pages = Page.root
   end
 
   def upload
-    # require 'csv'
-    # CSV.read(params['file'], headers: true) do |x|
-    #   p x
-    # end
+    require 'csv'
+    respond_to do |format|
+      Page.destroy_all
+      CSV.read(params['file'], headers: true).each do |row|
+
+        hashed = Page.determine_page_level(row['relative_path'])
+        if hashed[:level] == 'root' 
+            Page.create!(name: row['name'], code_path: '/', root: true)
+        elsif hashed[:level] == 'top'
+            root = Page.find_by_code_path('/')
+            Page.create!(name: row['name'], code_path: hashed[:value], root: false, page_id: root.id)
+        else
+            value = hashed[:value]
+            nested = Page.find_by_code_path(value.first)
+            Page.create!(name: row['name'], code_path: value.last, root: false, page_id: nested.id)
+        end
+      end
+      format.html {redirect_to root_path}
+    end
   end
 
   private
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,19 @@
 class Page < ApplicationRecord
     has_many :nested_pages, class_name: 'Page'
 
-    scope :root, -> {where(:root, true)}
+    scope :root, -> {where(root: true)}
+
+
+    def self.determine_page_level(string)
+        temp = string&.split('/')
+        if !temp
+            {level: 'root', value: []}
+        elsif temp.size == 2
+            {level: 'top', value: temp.last}
+        else
+            {level: 'nested', value: temp[1..-1]}
+        end
+           
+      end
 
 end
